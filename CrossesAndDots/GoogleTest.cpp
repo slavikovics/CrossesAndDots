72474b50420adcdf933f20f3caf4a928446d2cb8
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-09-28 00:18:37 +0300

    google test setup

---
+++ CrossesAndDots/GoogleTest.cpp
@@ -0,0 +1,7 @@
+#include<gtest/gtest.h>
+#define _SILENCE_TR1_NAMESPACE_DEPRECATIONWARNING
+
+TEST(,)
+{
+	EXPECT_EQ(1,1);
+}
---
+++ GoogleTest/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../CrossesAndDots/Player.cpp"
+
+TEST(GameFieldTest, ConstructorTest) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
