e270c074cfa8405a7971393e1a725415e2217499
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-09-28 01:56:31 +0300

    Added unit tests for ScenesBuilder::GameBuild()

--- MicrosoftUnitTests/MicrosoftUnitTests.cpp
+++ MicrosoftUnitTests/MicrosoftUnitTests.cpp
@@ -10,7 +10,7 @@ namespace MicrosoftUnitTests
 {
 	TEST_CLASS(GameFieldTests)
 	{
-	public:
+		public:
 		
 		/// <summary>
 		/// Testting constructor GameField(int dimension), GetDimension(), GetField(i, j)
@@ -91,4 +91,22 @@ namespace MicrosoftUnitTests
 		}
 
 	};
+
+	TEST_CLASS(ScenesBuilderTests)
+	{
+		TEST_METHOD(GameBuildTest)
+		{
+			GameField gameField(3);
+			gameField.SetCross(0, 0);
+			string resp = ScenesBuilder::GameBuild(gameField);
+			Assert::AreEqual(resp, (string)" X  #  # \n #  #  # \n #  #  # \n");
+
+			gameField.SetCross(0, 0);
+			gameField.SetCross(1, 1);
+			resp = ScenesBuilder::GameBuild(gameField);
+			Assert::AreEqual(resp, (string)" X  #  # \n #  X  # \n #  #  # \n");
+		}
+	};
+
+
 }
