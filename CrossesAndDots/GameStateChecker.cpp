4451d5c9b59a919834320acc9650d91b91b86437
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-09-27 20:51:11 +0300

    Changing structure of GameStateChecker

--- CrossesAndDots/GameStateChecker.cpp
+++ CrossesAndDots/GameStateChecker.cpp
@@ -1,1 +1,126 @@
 #include "GameStateChecker.h"
+
+GameStateChecker::GameStateChecker(int fieldDimension, GameField& gameField)
+{
+	_fieldDimension = fieldDimension;
+	_gameField = gameField;
+}
+
+string GameStateChecker::CheckVertical()
+{
+	int countInRow;
+
+	string winner;
+
+	for (int i = 0; i < _fieldDimension; i++)
+	{
+		winner = gameStates.StateNone;
+		countInRow = 0;
+
+		for (int j = 0; j < _fieldDimension; j++)
+		{
+			string currentField = _gameField.GetField2(i, j);
+			if (j == 0) winner = currentField;
+			if (winner == gameStates.StateNone) countInRow = -1;
+			if (winner != currentField) countInRow = -1;
+			countInRow++;
+		}
+
+		if (countInRow == _fieldDimension) return winner;
+	}
+
+	return gameStates.StateNone;
+}
+
+string GameStateChecker::CheckHorizontal()
+{
+	int countInRow;
+
+	string winner;
+
+	for (int i = 0; i < _fieldDimension; i++)
+	{
+		winner = gameStates.StateNone;
+		countInRow = 0;
+
+		for (int j = 0; j < _fieldDimension; j++)
+		{
+			string currentField = _gameField.GetField2(j, i);
+
+			if (j == 0) winner = currentField;
+			if (winner == gameStates.StateNone) countInRow = -1;
+			if (winner != currentField) countInRow = -1;
+			countInRow++;
+		}
+
+		if (countInRow == _fieldDimension) return winner;
+	}
+
+	return gameStates.StateNone;
+}
+
+string GameStateChecker::CheckD1()
+{
+	string winner = gameStates.StateNone;
+
+	for (int i = 0; i < _fieldDimension; i++)
+	{
+		string currentField = _gameField.GetField2(i, i);
+		if (i == 0) winner = currentField;
+		if (currentField == gameStates.StateNone) return gameStates.StateNone;
+		if (currentField != winner) return gameStates.StateNone;
+	}
+
+	return winner;
+}
+
+string GameStateChecker::CheckD2()
+{
+	string winner = gameStates.StateNone;
+
+	int x = 0;
+
+	for (int i = _fieldDimension - 1; i >= 0; i--)
+	{
+		string currentField = _gameField.GetField2(x, i);
+		if (x == 0) winner = currentField;
+		if (currentField == gameStates.StateNone) return gameStates.StateNone;
+		if (currentField != winner) return gameStates.StateNone;
+		x++;
+	}
+
+	return winner;
+}
+
+string GameStateChecker::CheckTie()
+{
+	for (int i = 0; i < _fieldDimension; i++)
+	{
+		for (int j = 0; j < _fieldDimension; j++)
+		{
+			if (_gameField.GetField2(i, j) == gameStates.StateNone) return gameStates.StateNone;
+		}
+	}
+
+	return gameStates.StateTie;
+}
+
+string GameStateChecker::CheckGameState()
+{
+	string state = CheckVertical();
+	if (state != gameStates.StateNone) return state;
+
+	state = CheckHorizontal();
+	if (state != gameStates.StateNone) return state;
+
+	state = CheckD1();
+	if (state != gameStates.StateNone) return state;
+
+	state = CheckD2();
+	if (state != gameStates.StateNone) return state;
+
+	state = CheckTie();
+	if (state != gameStates.StateNone) return state;
+
+	return gameStates.StateNone;
+}
--- CrossesAndDots/GameStateChecker.h
+++ CrossesAndDots/GameStateChecker.h
@@ -5,137 +5,25 @@ class GameStateChecker
 {
 	public:
 
-		GameStateChecker(int fieldDimension, GameField& gameField)
-		{
-			_fieldDimension = fieldDimension;
-			_gameField = gameField;
-		}
+		GameStateChecker(int fieldDimension, GameField& gameField);
 
-		string CheckVertical()
-		{
-			int countInRow;
-
-			string winner;
-
-			for (int i = 0; i < _fieldDimension; i++)
-			{
-				winner = gameStates.StateNone;
-				countInRow = 0;
-
-				for (int j = 0; j < _fieldDimension; j++)
-				{
-					string currentField = _gameField.GetField2(i, j);
-					if (j == 0) winner = currentField;
-					if (winner == gameStates.StateNone) countInRow = -1;
-					if (winner != currentField) countInRow = -1;
-					countInRow++;
-				}
-
-				if (countInRow == _fieldDimension) return winner;
-			}
-
-			return gameStates.StateNone;
-		}
-
-		string CheckHorizontal()
-		{
-			int countInRow;
-
-			string winner;
-
-			for (int i = 0; i < _fieldDimension; i++)
-			{
-				winner = gameStates.StateNone;
-				countInRow = 0;
-
-				for (int j = 0; j < _fieldDimension; j++)
-				{
-					string currentField = _gameField.GetField2(j, i);
-
-					if (j == 0) winner = currentField;
-					if (winner == gameStates.StateNone) countInRow = -1;
-					if (winner != currentField) countInRow = -1;
-					countInRow++;
-				}
-
-				if (countInRow == _fieldDimension) return winner;
-			}
-
-			return gameStates.StateNone;
-		}
-		
-		string CheckD1()
-		{
-			string winner = gameStates.StateNone;
-
-			for (int i = 0; i < _fieldDimension; i++)
-			{
-				string currentField = _gameField.GetField2(i, i);
-				if (i == 0) winner = currentField;
-				if (currentField == gameStates.StateNone) return gameStates.StateNone;
-				if (currentField != winner) return gameStates.StateNone;
-			}
-
-			return winner;
-		}
-
-		string CheckD2()
-		{
-
-			string winner = gameStates.StateNone;
-
-			int x = 0;
-
-			for (int i = _fieldDimension - 1; i >= 0; i--)
-			{
-				string currentField = _gameField.GetField2(x, i);
-				if (x == 0) winner = currentField;
-				if (currentField == gameStates.StateNone) return gameStates.StateNone;
-				if (currentField != winner) return gameStates.StateNone;
-				x++;
-			}
-
-			return winner;
-		}
-
-		string CheckTie()
-		{
-			for (int i = 0; i < _fieldDimension; i++)
-			{
-				for (int j = 0; j < _fieldDimension; j++)
-				{
-					if (_gameField.GetField2(i, j) == gameStates.StateNone) return gameStates.StateNone;
-				}
-			}
-
-			return gameStates.StateTie;
-		}
-
-		string CheckGameState()
-		{
-
-			string state = CheckVertical();
-			if (state != gameStates.StateNone) return state;
-
-			state = CheckHorizontal();
-			if (state != gameStates.StateNone) return state;
-
-			state = CheckD1();
-			if (state != gameStates.StateNone) return state;
-
-			state = CheckD2();
-			if (state != gameStates.StateNone) return state;
-
-			state = CheckTie();
-			if (state != gameStates.StateNone) return state;
-
-			return gameStates.StateNone;
-		}
+		string CheckGameState();
 
 	private:
 
 		GameStates gameStates;
+
 	    GameField _gameField;
+
 	    int _fieldDimension;
 
+		string CheckVertical();
+
+		string CheckHorizontal();
+
+		string CheckD1();
+
+		string CheckD2();
+
+		string CheckTie();
 };
