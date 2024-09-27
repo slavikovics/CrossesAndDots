4bd55863a61e7cb4a4d222092f6bce86c84ebe43
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-09-27 20:22:47 +0300

    Splitting GameField declaration and implementation

---
+++ CrossesAndDots/GameField.cpp
@@ -0,0 +1,105 @@
+#include "GameField.h"
+
+// Constructor for main instances
+GameField::GameField(int dimension)
+{
+	isACopy = false;
+	SetFieldDimension(dimension);
+}
+
+// Constructor for copied instances (for the field of GameStateChecker). Requires operator= call.
+GameField::GameField()
+{
+	fieldDimension = 0;
+	fields = NULL;
+	isACopy = true;
+}
+
+// Destructor is only called in main instances
+GameField::~GameField()
+{
+	if (isACopy) return;
+
+	for (int i = 0; i < fieldDimension; i++)
+	{
+		delete[] fields[i];
+	}
+
+	delete[] fields;
+}
+
+GameField& GameField::operator= (GameField& gameField)
+{
+	fieldDimension = gameField.GetDimension();
+	fields = gameField.fields;
+	return *this;	
+}
+
+void GameField::SetFieldDimension(int d)
+{
+	if (d < 2 || d > 20) d = 3;
+
+	fieldDimension = d;
+
+	fields = new int* [fieldDimension];
+
+	for (int i = 0; i < fieldDimension; i++)
+	{
+		fields[i] = new int[fieldDimension];
+	}
+
+	for (int i = 0; i < fieldDimension; i++)
+	{
+		for (int j = 0; j < fieldDimension; j++)
+		{
+			fields[i][j] = 0;
+		}
+	}
+}
+
+string GameField::GetField(int index)
+{
+	unsigned x = index % fieldDimension;
+	unsigned y = index / fieldDimension;
+	return GetField2(x, y);
+}
+
+int GameField::GetDimension()
+{
+	return fieldDimension;
+}
+
+string GameField::operator[](int x)
+{
+	return GetField(x);
+}
+
+string GameField::GetField2(int x, int y)
+{
+	if (x >= fieldDimension || y >= fieldDimension || x < 0 || y < 0) return "wrong arguments";
+	if (fields[x][y] == 1) return gameStates.StateCross;
+	if (fields[x][y] == 2) return gameStates.StateZero;
+	else return gameStates.StateNone;
+}
+
+bool GameField::SetZero(int y, int x)
+{
+	if (GetField2(x, y) == gameStates.StateNone)
+	{
+		fields[x][y] = 2;
+		return true;
+	}
+
+	return false;
+}
+
+bool GameField::SetCross(int y, int x)
+{
+	if (GetField2(x, y) == gameStates.StateNone)
+	{
+		fields[x][y] = 1;
+		return true;
+	}
+
+	return false;
+}
--- CrossesAndDots/GameField.h
+++ CrossesAndDots/GameField.h
@@ -4,129 +4,44 @@
 class GameField
 {
 
-   // 0 - no element
-   // 1 - cross
-   // 2 - zero
+	private:
 
-  public:
-	GameField(int dimension);
-	GameField();
-	~GameField();
+		int fieldDimension;
 
-	GameField& operator = (GameField& gameField)
-	{
-		fieldDimension = gameField.GetDimension();
-		fields = gameField.fields;
-		return *this;
-	}
+		int** fields;
 
-  private:
-	int fieldDimension;
-	int** fields;
-	bool isACopy;
-	GameStates gameStates;
+		bool isACopy;
 
-	// Sets game field dimension. Acceptable d values: d >= 2 && d <= 20. Default: 3.
+		GameStates gameStates;
 
-	void SetFieldDimension(int d)
-	{
-		if (d < 2 || d > 20) d = 3;
+		// Sets game field dimension. Acceptable d values: d >= 2 && d <= 20. Default: 3.
 
-		fieldDimension = d;
+		void SetFieldDimension(int d);
 
-		fields = new int*[fieldDimension];
-
-		for (int i = 0; i < fieldDimension; i++)
-		{
-			fields[i] = new int[fieldDimension];
-		}
-
-		for (int i = 0; i < fieldDimension; i++)
-		{
-			for (int j = 0; j < fieldDimension; j++)
-			{
-				fields[i][j] = 0;
-			}
-		}
-	}
-
-	string GetField(int index)
-	{
-		unsigned x = index % fieldDimension;
-		unsigned y = index / fieldDimension;
-		return GetField2(x, y);
-	}
+		string GetField(int index);
 
 	public:	
 
-	int GetDimension()
-	{
-		return fieldDimension;	
-	}
+		GameField(int dimension);
 
-	string operator[](int x)
-	{
-		return gameStates.StateNone;
-	}
+		GameField();
 
-	string GetField2(int x, int y)
-	{
-		if (x >= fieldDimension || y >= fieldDimension || x < 0 || y < 0) return "wrong arguments";
-		if (fields[x][y] == 1) return gameStates.StateCross;
-		if (fields[x][y] == 2) return gameStates.StateZero;
-		else return gameStates.StateNone;
-	}
+		~GameField();
 
-	bool SetZero(int y, int x)
-	{
-		if (GetField2(x, y) == gameStates.StateNone)
-		{
-			fields[x][y] = 2;
-			return true;
-		}
+		GameField& operator = (GameField& gameField);
 
-		return false;
-	}
+		int GetDimension();
 
-	// Method for setting a cross in a GameField. Tries to set cross in the given position. 
-	// Returns true if the cross was successfully set, 
-	// returns false if cross can't be set in the given position
-	bool SetCross(int y, int x)
-	{
-		if (GetField2(x, y) == gameStates.StateNone)
-		{
-			fields[x][y] = 1;
-			return true;
-		}
+		// TODO change implementation
+		string operator[](int x);
 
-		return false;
-	}
+		string GetField2(int x, int y);
+
+		bool SetZero(int y, int x);
+
+		// Method for setting a cross in a GameField. Tries to set cross in the given position. 
+		// Returns true if the cross was successfully set, 
+		// returns false if cross can't be set in the given position
+		bool SetCross(int y, int x);
 };
 
-// Constructor for main instances
-GameField::GameField(int dimension)
-{
-	isACopy = false;
-	SetFieldDimension(dimension);
-}
-
-// Constructor for copied instances (for the field of GameStateChecker). Requires operator= call.
-GameField::GameField()
-{
-	fieldDimension = 0;
-	fields = NULL;
-	isACopy = true;
-}
-
-// Destructor is only called in main instances
-GameField::~GameField()
-{
-	if (isACopy) return;
-	
-	for (int i = 0; i < fieldDimension; i++)
-	{
-		delete[] fields[i];
-	}
-	
-	delete[] fields;
-}
