1a37191612a9dcaad30e477d327943c6f31cd277
slavikovics <144949100+slavikovics@users.noreply.github.com>
2024-10-11 17:10:22 +0300

    Adding GameFieldTests to google test project

--- CrossesAndDotsTests/GameFieldTests.cpp
+++ CrossesAndDotsTests/GameFieldTests.cpp
@@ -13,3 +13,54 @@ TEST(GameFieldTest, ConstructorTest) {
 		}
 	}
 }
+
+TEST(GameFieldTest, GetField2Test)
+{
+	GameField gameField(5);
+	EXPECT_EQ(gameField.GetField2(10, 10), (string)"wrong arguments");
+
+	EXPECT_EQ(gameField.GetField2(-10, -10), (string)"wrong arguments");
+}
+
+TEST(GameFieldTest, OperatorTest)
+{
+	GameField gameField(5);
+	gameField[5] = 1;
+	EXPECT_EQ(gameField[5], 1);
+	gameField[5] = 312;
+	EXPECT_EQ(gameField[5], 312);
+	gameField[5] = -12;
+	EXPECT_EQ(gameField[5], -12);
+	gameField[5] = 0;
+	EXPECT_EQ(gameField[5], 0);
+}
+
+TEST(GameFieldTest, SetZeroTest)
+{
+	GameField gameField(3);
+	GameStates gameStates;
+	gameField.SetZero(0, 0);
+	EXPECT_EQ(gameField.GetField2(0, 0), gameStates.StateZero);
+
+	gameField.SetZero(1, 1);
+	EXPECT_EQ(gameField.GetField2(1, 1), gameStates.StateZero);
+
+	EXPECT_EQ(gameField.SetZero(-1, -1), false);
+
+	EXPECT_EQ(gameField.SetZero(10, 10), false);
+}
+
+TEST(GameFieldTest, SetCrossTest)
+{
+	GameField gameField(3);
+	GameStates gameStates;
+	gameField.SetCross(0, 0);
+	EXPECT_EQ(gameField.GetField2(0, 0), gameStates.StateCross);
+
+	gameField.SetCross(1, 1);
+	EXPECT_EQ(gameField.GetField2(1, 1), gameStates.StateCross);
+
+	EXPECT_EQ(gameField.SetCross(-1, -1), false);
+
+	EXPECT_EQ(gameField.SetCross(10, 10), false);
+}
